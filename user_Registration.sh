3be09973c46e5739c09a84b664105cefcf1781cb
pratikshad16 <dhagepratiksha16@gmail.com>
2020-07-10 20:46:49 +0530

    Added: Last name validation pattern

--- user_Registration.sh
+++ user_Registration.sh
@@ -1,12 +1,21 @@
 #!/bin/bash -x
 shopt -s extglob
 echo "Welcome to User Registration problem"
-echo "Enter the first name:"
-read name
-namePatt="^[A-Z]{1}[a-z]{2,}$"
-if [[ $name =~ $namePatt ]]
+namePatt="^[A-Z]{1}[a-z]{3,}$"
+function validate () {
+if [[ $1 =~ $2 ]]
 then
 	echo "Valid"
 else
-	echo "Invalid"
+	echo $3
 fi
+}
+echo "Enter the first name:"
+read firstName
+errorMsg="Invalid  First name:first Name Starts with Capital letter and minimum 3 chracters"
+validate $firstName $namePatt "$errorMsg"
+
+echo "Enter the last name:"
+read lastName
+errorMsg="Invalid  Last name: Last Name Starts with Capital letter and minimum 3 chracters"
+validate $lastName $namePatt "$errorMsg"
